e676356d9386a78a133934da195b4051c1cd6238
Aditya Sharma <adityashf7@gmail.com>
2023-07-15 00:00:52 +0530

    update

--- Day4/Problem3/MajorityElement.java
+++ Day4/Problem3/MajorityElement.java
@@ -25,3 +25,26 @@ public class MajorityElement {
         return element;
     }
 }
+
+// HashMap approach:
+
+public class MajorityElement {
+    public static void main(String[] args) {
+        int[] nums = {2,2,1,1,1,2,2};
+        int ans = majorityElement(nums);
+        System.out.println(ans);
+    }
+
+    public static int majorityElement(int[] nums) {
+        int n = nums.length;
+        Map<Integer, Integer> countMap = new HashMap<>();
+
+        for (int num : nums) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+            if (countMap.get(num) > n / 2) {
+                return num;
+            }
+        }
+        return -1; // No majority element found
+    }
+}

e45b1a51e12cce78566b365b9a980469dbb36289
Aditya Sharma <adityashf7@gmail.com>
2023-08-09 23:42:29 +0530

    update

---
+++ Day9/Problem1/NmeetingsInOneRoom.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class NmeetingsInOneRoom {
+    public static void main(String[] args) {
+        int[] start = {1,3,0,5,8,5};
+        int[] end = {2,4,6,7,9,9};
+        int n = 6;
+
+    }
+
+    public static int maxMeetings(int[] start, int[] end, int n){
+        
+    }
+}
